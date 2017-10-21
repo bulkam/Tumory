0cce334ada363b49e9ffe0e44d7f577ee4dddef6
bulkam <border45@seznam.cz>
2017-10-21 14:31:38 +0200

    run test bilateral

--- test_modules.py
+++ test_modules.py
@@ -22,8 +22,8 @@ def test_hogs():
     print len(data.orig_images)
     
     #print hog.dataset.load_annotated_images()
-    TM = hog.extract_features(to_save=bool(0), multiple_rois=bool(0), 
-                              PCA_partially=bool(0), save_features=bool(1))
+    TM = hog.extract_features(to_save=bool(1), multiple_rois=bool(1), 
+                              PCA_partially=bool(1), save_features=bool(0))
     
     return TM
 
