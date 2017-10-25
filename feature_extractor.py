4eaf20e9f518369dbab3f5dd928ea7e43a24fa05
bulkam <border45@seznam.cz>
2017-10-25 21:05:26 +0200

    testing median 15

--- feature_extractor.py
+++ feature_extractor.py
@@ -302,7 +302,7 @@ class Extractor(object):
         # bilatelarni transformace
         #out = cv2.bilateralFilter(out, 9, 35, 35)
         # median filter
-        out = cv2.medianBlur(out, 11)
+        out = cv2.medianBlur(out, 15)
         
         # vyuziti celeho histogramu
         # out = exposure.rescale_intensity(out)
