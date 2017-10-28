c35eef20253b11eb82ae6df8c7a912900c648dca
bulkam <border45@seznam.cz>
2017-10-28 11:30:51 +0200

    test coloring

--- feature_extractor.py
+++ feature_extractor.py
@@ -300,9 +300,9 @@ class Extractor(object):
         
         out = copy.copy(roi.astype("uint8"))
         # bilatelarni transformace
-        out = cv2.bilateralFilter(out, 9, 15, 15)
+        #out = cv2.bilateralFilter(out, 9, 15, 15)
         # median filter
-        #out = cv2.medianBlur(out, 15)
+        out = cv2.medianBlur(out, 13)
         
         # vyuziti celeho histogramu
         # out = exposure.rescale_intensity(out)
