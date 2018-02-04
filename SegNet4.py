472030af313c06a36ac13dc65484310107860153
bulkam <border45@seznam.cz>
2018-02-04 11:39:56 +0100

    call evaluation methods from module

--- SegNet4.py
+++ SegNet4.py
@@ -29,8 +29,7 @@ import numpy as np
 
 #import keras_data_reader as dr
 import file_manager_metacentrum as fm
-import CNN_evaluator
-import CNN_boxes_evaluator
+import CNN_experiment
 
 
 
@@ -167,41 +166,4 @@ fm.save_json(config, experiment_foldername+"/notebook_config.json")
 
 
 """ Ohodnoceni """
-
-# nacteni dat
-test_data = hdf_file['test_data']
-test_labels = hdf_file["test_labels"]
-
-# ohodnoceni Keras
-evaluation = model.evaluate(x=test_data, y=test_labels, batch_size=8)
-print(model.metrics_names, evaluation)
-# ulozeni do souboru
-eval_vocab = {}
-for i in range(len(model.metrics_names)):
-    eval_vocab[model.metrics_names[i]] = evaluation[i]
-fm.save_json(eval_vocab, experiment_foldername+"/model_evaluation.json")
-
-# ohodnoceni vlastnimi metrikami
-test_predicted_labels = model.predict(test_data, batch_size=8)
-
-my_eval_vocab = {}
-# accuracy per pixel
-ApP = CNN_evaluator.accuracy_per_pixel(test_labels, test_predicted_labels)
-my_eval_vocab["per_pixel_accuracy"] = ApP
-AMat_soft = CNN_evaluator.accuracy_matrix(test_labels, 
-                                          test_predicted_labels).tolist()
-my_eval_vocab["accuracy_matrix_soft": AMat_soft]
-AMat_onehot = CNN_evaluator.accuracy_matrix(test_labels, 
-                                            test_predicted_labels, 
-                                            mode="onehot").tolist()
-my_eval_vocab["accuracy_matrix_onehot": AMat_onehot] 
-# Jaccard similarity
-JS = CNN_evaluator.evaluate_JS(test_labels, test_predicted_labels)
-my_eval_vocab.update(JS)
-# HoGovsky evaluate
-_, _, _, _, boxes_eval = CNN_boxes_evaluator.evaluate_nms_results_overlap(test_data, 
-                                                 test_labels, 
-                                                 test_predicted_labels)
-my_eval_vocab.update({"boxes": boxes_eval})
-# ulozeni vysledku
-fm.save_json(my_eval_vocab, experiment_foldername+"/evaluation.json")
+CNN_experiment.evaluate_all(hdf_file, model, experiment_foldername)
