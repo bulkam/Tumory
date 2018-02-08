bc2d91d050dbeb913883a086e4d132746a9fa2d1
bulkam <border45@seznam.cz>
2018-02-08 11:01:21 +0100

    Using nesterov momentum

--- SegNet4.py
+++ SegNet4.py
@@ -127,7 +127,8 @@ model = Model(inputs=inputs, outputs=predictions)
 
 #optimizer = SGD(lr=LR)#, clipvalue=0.5)
 #optimizer = RMSprop(lr=LR, rho=0.9, decay=0.0)
-optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, decay=0.0)
+#optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, decay=0.0)
+optimizer = SGD(lr=LR, decay=1e-6, momentum=0.9, nesterov=True)
 
 model.compile(optimizer=optimizer,
               loss=loss,
@@ -139,7 +140,7 @@ print(model.summary())
 """ Sprava souboru """
 """ Pozor - jen pokud mam nejake specialni oznaceni """
 
-special_label = "Adam_SegNet4_LRdet_5epochs_weighted-01-35-4"
+special_label = "Nester_SegNet4_LRdet_5epochs_weighted-01-35-4"
 
 if len(special_label) >= 1:
     if not experiment_foldername.endswith(special_label):
